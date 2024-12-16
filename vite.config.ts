b3d0f887fe86b4b43512299b266443f2c27129d8
shirai <ryhshr3@gmail.com>
2024-12-17 01:13:54 +0900

    ♻️ chore: viteのremix.future指定

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,16 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
+        // https://remix.run/docs/en/2.13.1/start/future-flags#v3_fetcherpersist
         v3_fetcherPersist: true,
+        // https://remix.run/docs/en/2.13.1/start/future-flags#v3_relativesplatpath
         v3_relativeSplatPath: true,
+        // https://remix.run/docs/en/2.13.1/start/future-flags#v3_throwabortreason
         v3_throwAbortReason: true,
+        // https://remix.run/docs/en/2.13.1/start/future-flags#v3_singlefetch
+        v3_singleFetch: true,
+        // https://remix.run/docs/en/2.13.1/start/future-flags#v3_lazyroutediscovery
+        v3_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
