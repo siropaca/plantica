46db85eb070b37ab414d98539f191cec0d861ba5
shirai <ryhshr3@gmail.com>
2025-01-03 01:20:04 +0900

    perf: フォントの読み込み削除

--- app/root.tsx
+++ app/root.tsx
@@ -6,19 +6,6 @@ import unocss from '~/uno.css?url'
 export const meta: MetaFunction = () => [{ title: 'Plantica' }]
 
 export const links: LinksFunction = () => [
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.googleapis.com',
-  },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
   { rel: 'stylesheet', href: reset },
   { rel: 'stylesheet', href: unocss },
 ]
