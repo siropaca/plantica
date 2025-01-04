b5067d42c2e4058fb63215efe73238ac0b66c510
shirai <ryhshr3@gmail.com>
2025-01-05 00:10:04 +0900

    feat: vitest.setup.ts追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['app/test/vitest.setup.ts'],
     includeSource: ['app/**/*.{js,ts}'],
   },
   define: {
