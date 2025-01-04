2b01348015b42045fbcf37ad7d3a33f21eb1dc0f
shirai <ryhshr3@gmail.com>
2025-01-04 19:00:38 +0900

    refactor: Layout削除

--- app/root.tsx
+++ app/root.tsx
@@ -12,29 +12,23 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: unocss },
 ]
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  )
-}
-
 export default function App() {
   return (
     <Providers>
-      <Outlet />
+      <html lang="ja">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
     </Providers>
   )
 }
